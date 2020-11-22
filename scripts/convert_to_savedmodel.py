33e0d4b050d9d56bec3912452a5596ff0b4c30d6
박상준 <psj8252@naver.com>
2020-11-22 13:18:28 +0900

    fix: Difference sequence length error - when the number of data greater than batchsize

--- scripts/convert_to_savedmodel.py
+++ scripts/convert_to_savedmodel.py
@@ -61,20 +61,20 @@ if __name__ == "__main__":
         dataset = tf.data.Dataset.from_tensor_slices(tokens)
         dataset = dataset.map(lambda x: x)  # For using padded_batch from dataset made of RaggedTensor
         dataset = dataset.padded_batch(batch_size)
+        original_texts = tf.data.Dataset.from_tensor_slices(texts).batch(batch_size)
 
-        # Inference
-        space_mask = tf.constant((), dtype=tf.bool)
-        for data in dataset:
-            output = model(data)
-            space_mask = tf.reshape(space_mask, (-1, tf.shape(output)[1]))
-            space_mask = tf.concat((space_mask, output < threshold), 0)
+        all_spaced_sentences = tf.constant((), dtype=tf.string)
+        for texts, input_data in tf.data.Dataset.zip((original_texts, dataset)):
+            # Inference
+            output = model(input_data)
 
-        # Make spaced sentence
-        texts = tf.strings.unicode_split(texts, "UTF-8")
-        spaced_sentences = tf.where(space_mask, texts.to_tensor(), (texts + " ").to_tensor())
-        spaced_sentences = tf.strings.reduce_join(spaced_sentences, axis=1)
+            # Make spaced sentence
+            texts = tf.strings.unicode_split(texts, "UTF-8")
+            spaced_sentences = tf.where(output < threshold, texts.to_tensor(), (texts + " ").to_tensor())
+            spaced_sentences = tf.strings.reduce_join(spaced_sentences, axis=1)
+            all_spaced_sentences = tf.concat((all_spaced_sentences, spaced_sentences), axis=0)
 
-        return {"spaced_sentences": spaced_sentences}
+        return {"spaced_sentences": all_spaced_sentences}
 
     @tf.function(input_signature=[tf.TensorSpec((None, None), tf.int32)])
     def model_inference(tokens):
