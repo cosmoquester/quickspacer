3546ce7990b833ec8fc002e8d5864bd0b44143b2
박상준 <psj8252@naver.com>
2020-11-22 18:01:45 +0900

    fix: File open error in Windows

--- quickspacer/constant.py
+++ quickspacer/constant.py
@@ -7,5 +7,5 @@ DEFAULT_VOCAB_PATH = os.path.join(RESOURCE_PATH, "vocab.txt")
 DEFAULT_MODEL_PATH = os.path.join(RESOURCE_PATH, "default_saved_model")
 DEFAULT_OOV_INDEX = tf.constant(1, dtype=tf.int32)
 DEFAULT_SPACE_INDEX = tf.constant(2, dtype=tf.int32)
-with open(DEFAULT_VOCAB_PATH) as f:
+with open(DEFAULT_VOCAB_PATH, encoding="utf-8") as f:
     DEFAULT_VOCAB_SIZE = len(list(f))
