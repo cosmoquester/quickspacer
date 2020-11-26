800a6bf3fc4a0930e6effd3304724d4c8fe3461b
박상준 <psj8252@naver.com>
2020-11-26 23:13:45 +0900

    build: Support python3.6 and tensorflow 2.1

--- setup.py
+++ setup.py
@@ -1,13 +1,28 @@
 from setuptools import find_packages, setup
 
+
+with open("README.md") as f:
+    long_description = f.read()
+
 setup(
     name="quickspacer",
-    version="1.0.3",
+    version="1.0.4",
     description="Korean spacing correction model that aims for fast speed and moderate accuracy.",
-    python_requires=">=3.7",
-    install_requires=["tensorflow>=2.3.0"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.6",
+    install_requires=["tensorflow>=2.1"],
     url="https://github.com/psj8252/quickspacer.git",
     author="Park Sangjun",
+    keywords=["spacer", "korean"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Natural Language :: Korean",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing",
+    ],
     packages=find_packages(exclude=["tests"]),
     package_data={
         "quickspacer": [
